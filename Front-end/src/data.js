dcef1a4d8d7aea41739218f6c17ea75158077245
Aditya <59229338+adit121@users.noreply.github.com>
2024-09-02 20:30:38 +0530

    Add files via upload

---
+++ Front-end/src/data.js
@@ -0,0 +1,9 @@
+let movies = ['Deadpool & Wolverine' , 'Stree 2-Sarkate ka aatank', 'Despicable Me 4 ', 'Joker:Folie a Deux','Gangs of Wasseypur']
+
+let slots =['9:00 AM' , '11:00 AM' , '12:00 PM' , '1:00 PM' , '3:00 PM' , '6:00 PM' , '9:00 PM' ]
+
+let seats =['A1','A2','A3', 'A4', 'D1','D2'];
+
+export  {movies}
+export {slots}
+export {seats}   
